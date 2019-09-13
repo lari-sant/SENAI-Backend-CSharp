44821af4394b9b54290e602f4ab0c431dc30ede7
FIC <FIC@info.local>
2019-09-13 15:06:31 -0300

    Atividade 3

---
+++ atividade3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace atividade3
+{
+    class Program
+    {
+        static void Main(string[] args)   
+        {
+            float salariofixo;
+            int totalv;
+            float salariototal;
+            float total;
+            float comissao;
+        
+
+            Console.WriteLine("Escreva seu salario fixo:");
+            salariofixo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escreva seu total de vendas: ");
+            totalv = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escreva o percentual de comissao ");
+            comissao = int.Parse(Console.ReadLine());
+            total = (salariofixo + totalv) *comissao/100;
+            salariototal = salariofixo + total; 
+            Console.WriteLine("O salario total é " + salariototal);
+
+
+
+        }
+    }
+}

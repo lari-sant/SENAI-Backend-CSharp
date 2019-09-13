39cf1d749bf96ca3157136785321db15f320480c
FIC <FIC@info.local>
2019-09-13 15:07:39 -0300

    Atividade 2

---
+++ atividade2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace atividade2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float percentual;
+            float mercadoria;
+            float acrescimo;
+
+            Console.WriteLine("Digite o custo da mercadoria: ");
+
+            mercadoria = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite valor do acrescimo: ");
+            acrescimo = int.Parse(Console.ReadLine());
+
+            percentual = (mercadoria/100) * acrescimo;
+
+            Console.WriteLine("Valor total com acrescimo de " + percentual);
+
+
+        }
+    }
+}

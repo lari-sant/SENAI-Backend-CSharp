7050006ec2aebdf8ef7ac52e34ec488b646268c8
FIC <FIC@info.local>
2019-09-13 15:03:16 -0300

    Aula 1

---
+++ atividade4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace atividade4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double real, dolar, conversao;
+
+            Console.WriteLine("Digite o valor em reais ");
+            real = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor do dolar no momento");
+            dolar = double.Parse(Console.ReadLine());
+            conversao = real * dolar;
+            Console.WriteLine("O valor da conversão é: " + conversao);
+
+
+        }
+    }
+}

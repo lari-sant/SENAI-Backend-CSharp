cc8e6a0af2f2f06b7023853db74e641e1170aec7
FIC <FIC@info.local>
2019-09-16 14:07:03 -0300

    Aula 2-3, adicionada

---
+++ aula2-3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace aula2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deixamos declarada como falsa por padrão
+            bool entrada = false;
+            int idade;
+            string retorno;
+
+            Console.WriteLine("Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            // Como já atribuimos anteriormente, mudamos o 
+            // estado da variavel para verdadeira caso
+            // entre nessa condição
+            if(idade >17 ){
+                entrada = true;
+            }
+
+            if( entrada == true ){
+                retorno= "Entrada Permitida!!!";
+            }else{
+                retorno = "Entrada não permitida :( ";
+            }
+            
+            Console.WriteLine(retorno);
+        }
+    }
+}

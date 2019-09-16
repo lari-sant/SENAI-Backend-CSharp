02ea9616cf10443f3912f3a586a24c756d9036b6
FIC <FIC@info.local>
2019-09-16 09:57:26 -0300

    Atividade 6

---
+++ atividade6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace atividade6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sf, contas, sobra;
+
+            Console.WriteLine("Digite o seu salario");
+            sf = double.Parse(Console.ReadLine());
+            Console.WriteLine("Valor de contas a pagar");
+            contas = double.Parse(Console.ReadLine());
+            sobra =  sf - contas;
+            Console.WriteLine("Sobrou R$  " + sobra + " do seu salario apos pagar dividas");
+
+        }
+    }
+}

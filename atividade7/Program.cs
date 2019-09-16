18c924981fe8f0c1a38603d9ea082a043dfce1f5
FIC <FIC@info.local>
2019-09-16 10:00:34 -0300

    Atividade 7

---
+++ atividade7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace atividade7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double passagem, dinherio_arrecadado;
+            int passageiros;
+            Console.WriteLine("Digite o preço da passagem R$ ");
+            passagem = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade de passageiros do onibus: ");
+            passageiros = int.Parse(Console.ReadLine());
+            dinherio_arrecadado = passagem * passageiros;
+            Console.WriteLine("Foi arrecadado R$ " + dinherio_arrecadado);
+
+        }
+    }
+}

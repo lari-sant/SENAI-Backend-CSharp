6d3cfc52fdaaddc0a92dd4434e71fb2013347f5c
FIC <FIC@info.local>
2019-09-26 10:09:54 -0300

    Aula 8 - Correção de erros

---
+++ aula8-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace aula8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {   
+            int numero;
+           try{
+
+               //Escrevemos um bloco de codico
+            Console.WriteLine("Digite um numero");
+            numero = int.Parse(Console.ReadLine());
+           }catch(Exception ex){
+               
+               //Mostramos a exceção da regra
+               //Erro gerado na aplicação
+               Console.WriteLine("Ops :( Deu ruim. Erro: " + ex);
+           }
+        }
+    }
+}
